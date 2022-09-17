62cd856473546968a65cbba72427963d489ead12
Oluwadamilare2003 <oluwadamilareobalana@gmail.com>
2022-09-17 15:13:05 +0100

    Update to my javascript

--- scripts/date.js
+++ scripts/date.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const option = {month: 'numeric', day: 'numeric', year: 'numeric'};
 
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', option);
