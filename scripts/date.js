569489543e04a1dc4e356f71a236fef261fbc9b4
Oluwadamilare2003 <oluwadamilareobalana@gmail.com>
2022-09-17 15:21:14 +0100

    Update to my HTML and Javascript to display year

--- scripts/date.js
+++ scripts/date.js
@@ -1,5 +1,8 @@
 "use strict"
 
 const option = {month: 'numeric', day: 'numeric', year: 'numeric'};
-
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', option);
+
+const todaydate = new Date();
+const year = todaydate.getFullYear();
+document.getElementById('year').textContent = year
