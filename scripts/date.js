a7ca576e3ba3af5f17d3aa429306ad08b97b0fb3
Oluwadamilare2003 <oluwadamilareobalana@gmail.com>
2022-10-01 15:27:53 +0100

    Updating my week 3 assignment

--- scripts/date.js
+++ scripts/date.js
@@ -11,3 +11,8 @@ document.getElementById('year').textContent = year
 const lesson_2 = document.querySelector("a");
 lesson_2.textContent = "Design Principles"
 lesson_2.href = "lesson_2/design-principles.html"
+
+// Changes the detail of the Lesson 3 link in HTML
+const lesson_3 = document.querySelector(".lesson_3")
+lesson_3.textContent = "Website Planning Document"
+lesson_3.href = "lesson_3/index.html"
