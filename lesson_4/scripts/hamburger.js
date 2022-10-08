e24c3e6f0c2b76c6bb5a1b3cc329fef0c663214c
Oluwadamilare2003 <oluwadamilareobalana@gmail.com>
2022-10-08 14:26:23 +0100

    Added the hamburger menu and the present date

--- lesson_4/scripts/hamburger.js
+++ lesson_4/scripts/hamburger.js
@@ -10,6 +10,7 @@ document.getElementById("lastModified").textContent = lastModified
 // Hamburger
 function toggleMenu(){
     document.getElementById("mainNavigation").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open")
 }
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu
