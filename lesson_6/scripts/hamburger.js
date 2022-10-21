8fc87a109a8d42abd14f0a66b9f28ad6a9cf3d3f
Oluwadamilare2003 <oluwadamilareobalana@gmail.com>
2022-10-21 21:29:15 +0100

    Modified the JavaScript

---
+++ lesson_6/scripts/hamburger.js
@@ -0,0 +1,19 @@
+// Today's date
+const today = new Date();
+const options = {weekday:"long",day:"numeric",month:"long",year:"numeric"}
+document.getElementById("current_date").textContent = today.toLocaleDateString("en-US", options)
+
+// Last Modified
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified
+
+// Hamburger
+function toggleMenu(){
+    document.getElementById("mainNavigation").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open")
+}
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu
+
+const current_year = document.getElementById("currentYear");
+current_year.textContent = new Date().getFullYear();
