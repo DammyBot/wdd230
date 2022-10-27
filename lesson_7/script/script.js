2821416d728f5eef55aa62d419914a8ee3f477c8
Oluwadamilare2003 <oluwadamilareobalana@gmail.com>
2022-10-27 22:51:05 +0100

    Lesson 7 assignment

---
+++ lesson_7/script/script.js
@@ -0,0 +1,26 @@
+const imageData = document.querySelectorAll("img[data-src]");
+
+function conversion(image) {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+}
+
+if ("IntersectionObserver" in window){
+    const observer = new IntersectionObserver((items,observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting){
+                conversion(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imageData.forEach((item)=>{
+        observer.observe(item)
+    })
+}else{
+    imageData.forEach((image) => {
+        conversion(image);
+    })
+}
