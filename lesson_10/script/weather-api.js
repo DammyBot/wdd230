be17d7be1f6803c78cce96fb12e24e14c0089bff
Oluwadamilare2003 <oluwadamilareobalana@gmail.com>
2022-11-19 10:59:39 +0100

    updated assignment

--- lesson_10/script/weather-api.js
+++ lesson_10/script/weather-api.js
@@ -4,26 +4,22 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 // URL and container
-const url = "https://api.openweathermap.org/data/3.0/onecall?lat=64.8401&lon=147.7200&exclude=daily,hourly&appid=15b3c845efce804c56e0ccf9cfedc22f"
+const url = "https://api.openweathermap.org/data/3.0/onecall?lat=64.835365&lon=-147.776749&exclude=daily,hourly&units=imperial&appid=15b3c845efce804c56e0ccf9cfedc22f"
 let details = [];
-console.log(details)
 
 // Async function
-async function conversion(url){
+async function conversion(url,array){
     const data = await fetch(url);
     if(data.ok){
         const converted = await data.json();
-        details = converted
-        // console.log(details);
-        // console.log(details.current.weather[0]);
-        displayResults(details.current);
+        array = converted
+        displayResults(array.current);
     }
 }
 
 function displayResults(data){
     const temp = parseInt(data.temp);
-    const cel = (temp - 273.15) * 9/5 + 32;
-    const fara = (cel * 9/5) + 32
+    currentTemp.textContent = temp.toFixed(1);
 
     const image = data.weather[0].icon;
     const imgUrl = `https://openweathermap.org/img/w/${image}.png`
@@ -31,9 +27,6 @@ function displayResults(data){
     weatherIcon.setAttribute("src",imgUrl);
     weatherIcon.setAttribute("alt",description);
     captionDesc.textContent = description;
-
-
-    currentTemp.textContent = fara.toFixed(2);
 }
 
-conversion(url);
+conversion(url,details);
