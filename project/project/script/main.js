71b9eeb50fafc85258ffc4d6d3009305255c5b3e
Oluwadamilare2003 <oluwadamilareobalana@gmail.com>
2022-12-13 19:51:32 +0100

    small update

--- project/project/script/main.js
+++ project/project/script/main.js
@@ -14,7 +14,7 @@ const textAnimation = new IntersectionObserver((items,observer)=>{
             item.target.classList.toggle("reposition", item.isIntersecting);
     })
 },{
-    threshold:.4,
+    threshold:.2,
 })
 const text = document.querySelector(".text");
 textAnimation.observe(text)
@@ -37,6 +37,9 @@ imagesAni.forEach(image=>{
 const fresh = document.querySelector(".drink")
 textAnimation.observe(fresh);
 
+const weather_card = document.querySelector(".weather_card");
+textAnimation.observe(weather_card)
+
 
 // Header Change on Scroll
 const heroImg = document.querySelector(".hero");
@@ -51,6 +54,7 @@ const headerObserver = new IntersectionObserver((items)=>{
     })
 }, {
     rootMargin: "-100px 0px 0px 0px",
+    threshold: .2,
 })
 headerObserver.observe(heroImg);
 
@@ -88,9 +92,30 @@ async function weatherDataFetch(url){
     if(data.ok){
         const converted = await data.json();
         weatherInformation = converted;
+        updateDetail(weatherInformation);
         console.log(weatherInformation);
     }
 }
+
+function updateDetail(data){
+    const temperature = document.querySelector(".temp");
+    temperature.textContent = data.current.temp;
+
+    const description = document.querySelector(".description");
+    const content_description = data.current.weather[0].description;
+    description.textContent = content_description;
+
+    const humidity = document.querySelector(".humidity");
+    humidity.textContent = data.current.humidity;
+
+    const weather_card = document.querySelector(".weather_card");
+    const card_url = data.current.weather[0].icon;
+    const id = data.current.weather[0].id;
+    const url = `http://openweathermap.org/img/w/${card_url}.png`
+    // 10d.png
+    weather_card.setAttribute("src", url)
+    weather_card.setAttribute("alt", content_description);
+};
 
 weatherDataFetch(weatherUrl);
 
@@ -112,7 +137,7 @@ const imageObserver = new IntersectionObserver((items, observer)=>{
         }
     })
 },{
-    threshold:.4,
+    threshold:0,
 });
 
 const images = document.querySelectorAll(".image");
